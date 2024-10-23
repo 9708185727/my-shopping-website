c9f7f351d43a8dd2969ab7c9695bd672eeaca9d2
raman <ramancdry2058@gmail.com>
2024-10-23 13:34:22 +0545

    update in home and sectc

--- src/components/home/SectC.jsx
+++ src/components/home/SectC.jsx
@@ -14,24 +14,25 @@ const SectC = ({ MenuData, selectedCategory }) => {
           selectedCategory === "All" ? "All Products" : ` ${selectedCategory}`
         }
       />
-      <div className="mx-16 p-2  grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4  space-x-2 rounded-lg">
+      <div className="mx-16 p-2 grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4  space-x-2 rounded-lg">
         {MenuData.map((menu) => {
           return (
             <div
-              key={menu._id}
+            key={menu._id}
               className="ms-2 my-3   shadow-lg max-w-auto max-h-auto rounded-lg cursor-pointer hover:scale-105 hover:translate hover:duration-75 hover:ease-in-out"
             >
-              <div className=" bg-teal-300  text-center p-2 rounded-lg ">
+              <div className="relative bg-teal-300 h-36  text-center p-2 rounded-lg ">
                 <img
                   src={menu.url}
                   alt="Error"
-                  className=" rounded-full max-w-24 max-h-24  mx-16 "
+                  className="h-24 rounded-full  w-24 mx-16 "
                 />
-                <p className="ms-2 my-3 p-2 text-black rounded-full float-left mb-56 ">
+                <p className="ms-2 my-2 p-1 absolute top-24 text-black rounded-full float-left mb-56 ">
                   {menu.category}
                   <IoIosStarOutline className="inline ms-1 mb-1 text-red-500" />
                 </p>
               </div>
+  
               <h2 className="ms-2 my-2 text-xl font-semibold ">{menu.name}</h2>
               <p className="my-4">
                 <span className="text-2xl font-serif ">
@@ -41,6 +42,7 @@ const SectC = ({ MenuData, selectedCategory }) => {
                   ${menu.price}
                 </span>
               </p>
+        
               <p className="text-slate-700 ml-1 my-2 p-1">
                 <LuBus className="inline mr-2 text-zinc-900" />
                 Free Delivery
@@ -49,7 +51,7 @@ const SectC = ({ MenuData, selectedCategory }) => {
                 {menu.rating}
                 <IoIosStarOutline className="inline ms-1 mb-1 text-red-500" />
               </p>{" "}
-            </div>
+              </div>
           );
         })}
       </div>
